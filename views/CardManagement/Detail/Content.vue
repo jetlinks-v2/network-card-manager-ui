61c2f1ae778f2ef41a7c92fee25da0386a2d41cc
XieYongHong <18010623010@163.com>
2025-10-24 13:38:14 +0800

    fix: 修复详情页中，物联卡的运营商定位没有展示数据

--- views/CardManagement/Detail/Content.vue
+++ views/CardManagement/Detail/Content.vue
@@ -202,9 +202,12 @@
               --
             </template>
           </a-descriptions-item>
-          <a-descriptions-item :label="$t('Detail.index.427958-28')">{{
-              marks[0]?.longitude &&marks[0]?.latitude ? `${marks[0]?.location}` : '--'
-            }}
+          <a-descriptions-item :label="$t('Detail.index.427958-28')">
+            <j-ellipsis>
+              {{
+                marks[0]?.longitude &&marks[0]?.latitude ? `${marks[0]?.location}` : '--'
+              }}
+            </j-ellipsis>
           </a-descriptions-item>
           <a-descriptions-item :label="$t('Detail.index.427958-15')">{{
               detail?.describe
